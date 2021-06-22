7d06a17bad25c7f102839f85a9f736ce779ca6de
Dongmin Kim <kdmin0211@gmail.com>
2021-06-22 22:53:23 +0900

    modify

--- colorball.cpp
+++ colorball.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
-
-int main(){
-    int ball_number, duplexprocessing=0; //공의 갯수, 예외처리
-    cin>>ball_number; 
-    vector<vector <int>> v(ball_number, vector<int> (2,0)); //공의 정보 저장하는 vector
-    vector<int> opervec(ball_number, 0); //결과를 저장하는 vector
-    vector<int> color_index(200001,0); //color 중복처리 vector
-    for(int temp =0;temp<ball_number;temp++){
-        cin>>v[temp][1]>>v[temp][0];
+const int MN = 200000 + 1;
+int N, accul, colorSum[MN], ret[MN];
+struct Ball { int c, s, idx; } ball[MN];
+bool COMP(const Ball &a, const Ball &b) { return a.s < b.s; }
+int main() {
+    scanf("%d", &N);
+    for (int i = 0; i < N; ++i) {
+        scanf("%d %d", &ball[i].c, &ball[i].s); ball[i].idx = i;
     }
-    sort(v.begin(),v.end());
-    color_index[v[0][1]]=v[0][0];
-    for(int temp=1;temp<ball_number;temp++){
-        opervec[temp]=opervec[temp-1]+v[temp-1][0]-color_index[v[temp][1]]+duplexprocessing; //해당 공 score계산
-        duplexprocessing=color_index[v[temp][1]]; //예외처리 값 초기화
-        color_index[v[temp][1]]+=v[temp][0]; //color 중복처리 vector 갱신
+    sort(ball, ball + N, COMP);
+    for (int i = 0, j = 0; i < N; ++i){
+        for (; ball[i].s > ball[j].s; ++j) {
+            accul += ball[j].s;
+            colorSum[ball[j].c] += ball[j].s;
+        }
+        ret[ball[i].idx] = accul - colorSum[ball[i].c];
     }
-    for(int temp =0 ; temp<ball_number ; temp++){
-        cout<<opervec[temp]<<endl;
-    }
-
-    
+    for (int i = 0; i < N; ++i) printf("%d\n", ret[i]);
+    return 0;
 }
